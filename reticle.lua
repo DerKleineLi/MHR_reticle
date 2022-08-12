b48df789429382b0f0c7749e4e5395101a7a813b
DerKleineLi <lhx393264301@163.com>
2022-08-13 01:01:32 +0200

    add weapon drawn detection, now support different color for weapon on/off.

--- reticle.lua
+++ reticle.lua
@@ -10,7 +10,8 @@ local cfg = json.load_file("reticle_settings.json")
 cfg = cfg or {}
 cfg.enabled = cfg.enabled or true
 cfg.size = cfg.size or 1
-cfg.color = cfg.color or 0xFFFFFF00
+cfg.color_weapon_off = cfg.color_weapon_off or 0x80FFFF00
+cfg.color_weapon_on = cfg.color_weapon_on or 0xFFFFFF00
 
 re.on_config_save(
     function()
@@ -24,7 +25,15 @@ function draw_reticle()
         if not size then return end
         local screen_w = size:get_field("w")
         local screen_h = size:get_field("h")
-        draw.filled_circle((screen_w+1)/2, (screen_h+1)/2, cfg.size, cfg.color, 30)
+        local player_manager = sdk.get_managed_singleton("snow.player.PlayerManager");
+        local master_player = player_manager:call("findMasterPlayer");
+        local weapon_drawn = master_player:call("isWeaponOn")
+        if weapon_drawn then 
+            draw.filled_circle((screen_w+1)/2, (screen_h+1)/2, cfg.size, cfg.color_weapon_on, 30)
+        else
+            draw.filled_circle((screen_w+1)/2, (screen_h+1)/2, cfg.size, cfg.color_weapon_off, 30)
+        end
+        
     end
 end
 
@@ -44,8 +53,17 @@ re.on_draw_ui(
         changed, value = imgui.slider_int("Size", cfg.size, 1, 20)
         if changed then cfg.size = value end
 
-        changed, value = imgui.color_picker("Color", cfg.color, "color for reticle")
-        if changed then cfg.color = value end
+        if imgui.tree_node("Color when weapon is off") then
+            changed, value = imgui.color_picker("Color", cfg.color_weapon_off, "color for reticle")
+            if changed then cfg.color_weapon_off = value end
+            imgui.tree_pop()
+        end
+
+        if imgui.tree_node("Color when weapon is on") then
+            changed, value = imgui.color_picker("Color", cfg.color_weapon_on, "color for reticle")
+            if changed then cfg.color_weapon_on = value end
+            imgui.tree_pop()
+        end
 
         imgui.tree_pop()
     end
